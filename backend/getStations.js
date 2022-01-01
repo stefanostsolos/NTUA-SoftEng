3b909daa37c77e4bf45b3e13f2a5013c5918e243
Alexandros Kouridakis <alkouridakis@gmail.com>
2022-01-01 20:39:48 +0200

    Added getStations.js

---
+++ backend/getStations.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const db = require('./db');
+const aux = require('./helper');
+
+const router = express.Router();
+
+// {baseURL}/GetStations
+router.get('/', async function (req, res, next) {
+    try {    
+        const request_timestamp = aux.get_current_timestamp();
+
+        const sql = 'SELECT ID FROM station';
+        [rows] = await db.execute(sql);
+
+        let id_list = [];
+        for (row of rows) {
+            id_list.push(row.ID);
+        }
+
+        res.status(200).send({
+            RequestTimestamp: request_timestamp,
+            ID_List: id_list
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+module.exports = router;
