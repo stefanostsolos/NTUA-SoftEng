a7e6643181deddb88bfb1bfbcb7ad074443e7f3e
Stefanos Tsolos <43885015+stefanostsolos@users.noreply.github.com>
2022-02-12 19:02:41 +0200

    Update settlementsbyoperator.js

--- cli/utils/settlementsbyoperator.js
+++ cli/utils/settlementsbyoperator.js
@@ -34,11 +34,11 @@ async function settlementsbyoperator(baseURL, token, op1, format) {
     let res;
 
     if (op1 == undefined) {
-        console.log("Error: operator one is missing");
+        console.log("Operator is missing");
         op1 = await promptMissingOperator();
     }
     if (format == undefined) {
-        console.log("Error: format is missing");
+        console.log("format is missing");
         format = await promptMissingFormat();
     }
 
