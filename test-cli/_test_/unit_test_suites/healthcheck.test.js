62ea556c8c436cebdce2aab2aebce6d3033d3c32
Stefanos Tsolos <43885015+stefanostsolos@users.noreply.github.com>
2022-02-19 18:03:23 +0200

    Delete healthcheck.test.js

--- test-cli/_test_/unit_test_suites/healthcheck.test.js
+++
@@ -1,7 +0,0 @@
-const baseURL = 'http://localhost:9103/interoperability/api';
-const healthcheck = require('./../../../CLI/admin/healthcheck.js')
-
-test('Unit Test for Healthcheck function', async () => {
-    const res = await healthcheck.healthcheck(baseURL);
-    expect(res).toBe(200)
-})
